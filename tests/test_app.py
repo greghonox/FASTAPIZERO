26880644250c02384b0c992b01cdbeee2abd3e29
Gregorio Honorato <greghono@gmail.com>
2023-08-08 22:56:24 -0300

    feat: first comment
    - preper struct

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+from fastapizero.app import app
+
+client = TestClient(app)
+
+
+def test_root_deve_retornar_200_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
