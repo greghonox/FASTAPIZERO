f8887c0c1f6927abd1d96dd38f62c3d48427e7a1
Gregorio Honorato <greghono@gmail.com>
2023-08-14 22:36:26 -0300

    feat: routes functional

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from .schemas import UserDB, UserList, UserPublic, UserSchema
 
@@ -6,7 +6,7 @@ app = FastAPI()
 
 
 @app.get('/')
-def read_root():
+async def read_root():
     return {'message': 'Olá Mundo!'}
 
 
@@ -14,7 +14,7 @@ database = []  # provisório para estudo!
 
 
 @app.post('/users/', status_code=201, response_model=UserPublic)
-def create_user(user: UserSchema) -> UserPublic:
+async def create_user(user: UserSchema) -> UserPublic:
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
 
     database.append(user_with_id)
@@ -23,5 +23,26 @@ def create_user(user: UserSchema) -> UserPublic:
 
 
 @app.get('/users/', status_code=200, response_model=UserList)
-def get_users():
+async def get_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+async def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}')
+async def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[user_id - 1]
+
+    return {'detail': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -23,3 +23,41 @@ async def test_create_user(client):
         'username': 'alice',
         'email': 'alice@example.com',
     }
+
+
+@pytest.mark.asyncio
+async def test_update_user(client):
+    client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {'username': 'bob', 'email': 'bob@example.com'}
+
+
+@pytest.mark.asyncio
+async def test_delete_user(client):
+    client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'detail': 'User deleted'}
